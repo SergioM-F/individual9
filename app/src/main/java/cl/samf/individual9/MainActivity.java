9ea9995762b9e4105668171de91bfd792318a886
Sergio <sergiomolinaflores1@gmail.com>
2023-06-23 21:53:38 -0400

    Individual 9 Funcionando Ok!

--- app/src/main/java/cl/samf/individual9/MainActivity.java
+++ app/src/main/java/cl/samf/individual9/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,44 +19,63 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
     }
 
+
+
     private void initListener() {
+        int imag1 = R.drawable.baseline_surfing_24;
+        int imag2 = R.drawable.baseline_pets_24;
+        int imag3 = R.drawable.baseline_sports_tennis_24;
+        int imag4 = R.drawable.baseline_personal_injury_24;
+
         ImageButton imagen1 = findViewById(R.id.imagebutton1);
         ImageButton imagen2 = findViewById(R.id.imageButton2);
         ImageButton imagen3 = findViewById(R.id.imageButton3);
         ImageButton imagen4 = findViewById(R.id.imageButton4);
+        Button botonSalir = findViewById(R.id.buttonSalir);
 
         imagen1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent1 = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent1);
+                moverActivity2(imag1);
             }
         });
 
         imagen2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent2 = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent2);
+                moverActivity2(imag2);
                 }
             });
 
         imagen3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent3 = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent3);
+                moverActivity2(imag3);
             }
         });
 
         imagen4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent4 = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent4);
+                moverActivity2(imag4);
             }
         });
+
+        botonSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+    }
+    public void moverActivity2(int img) {
+        Intent mover = new Intent(MainActivity.this, MainActivity2.class);
+        mover.putExtra("clave",img);
+        startActivity(mover);
+
     }
 }
--- app/src/main/java/cl/samf/individual9/MainActivity2.java
+++ app/src/main/java/cl/samf/individual9/MainActivity2.java
@@ -2,9 +2,11 @@ package cl.samf.individual9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -15,7 +17,11 @@ public class MainActivity2 extends AppCompatActivity {
 
         ListnerActivity2();
 
-        onBackPressed();
+        Intent intentget = getIntent();
+        int idImagen = intentget.getIntExtra("clave",0);
+
+        ImageView imagebutton = findViewById(R.id.imageView);
+        imagebutton.setImageResource(idImagen);
     }
 
     private void ListnerActivity2() {
